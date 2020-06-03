32e11e777103cce5b320dbed4e396c95b522632e
zhangsong <zhangsong8971719@126.com>
2020-06-03 10:21:20 +0800

    修改预览中图片样式 预览界面ios显示异常

--- src/components/content/preview.vue
+++ src/components/content/preview.vue
@@ -49,8 +49,8 @@ export default {
 <style scoped>
 .preview { padding: 0; display: flex; flex-direction: column; }
 .preview-header { font-weight: bold; height: 2.34375rem; line-height: 2.34375rem; border-bottom: 1px solid #e6e6e6; font-size: .5625rem; padding-left: 1.125rem; }
-.preview-body { flex: 1; height: 0; width: 100%; text-align: center; }
-.phone { width: 377px; height: 669px; margin: auto; border: 1px solid #333333; color: #282828; padding: 16px 14px 14px; box-sizing: border-box; text-align: left; overflow-y: scroll; }
+.preview-body { flex: 1; height: 0; width: 100%; text-align: center; overflow: hidden; overflow-y: scroll; }
+.phone { width: 377px; height: 669px; margin: auto; border: 1px solid #333333; color: #282828; padding: 16px 14px 14px; box-sizing: border-box; text-align: left; overflow: hidden; overflow-y: scroll; font-family: PingFang SC; }
 .phone::-webkit-scrollbar { display: none; }
 .phone>div { max-width: 100% !important; overflow: hidden; }
 .phone>div>div { max-width: 100% !important; overflow: hidden; }
@@ -59,12 +59,14 @@ export default {
 .pub-date { flex: 1; width: 0; height: 100%; text-align: left; }
 .read-num { height: 100%; width: auto; position: relative; }
 .read-num::before { content: ' '; background-image: url(http://static.yunzhanxinxi.com/find_ic_read@2x.png); background-repeat: no-repeat; background-size: 100% 100%; background-position: center center; display: block; position: absolute; width: 12px; height: 7.875px; left: -16px; top: 4px; }
+.article-video { width: 100%; }
+video { width: 100%; object-fit: fill; }
+.preview-footer { width: 100%; height: 1.25rem; line-height: 1.25rem; text-align: center; }
+</style>
+<style>
 .article-content { width: 100%; margin-top: 27px; word-break: break-all; font-size: 14px; line-height: 26px; }
 .article-content img { width: 100%; }
 .article-content>div { width: 100%; }
 .article-content>div,.article-content>img { margin-top: 25px; }
 .article-content>div:first-child,.article-content>img:first-child { margin-top: 0; }
-.article-video { width: 100%; }
-video { width: 100%; object-fit: fill; }
-.preview-footer { width: 100%; height: 1.25rem; line-height: 1.25rem; text-align: center; }
 </style>
